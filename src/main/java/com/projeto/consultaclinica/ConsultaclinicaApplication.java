15bad9dc612e437f8a660a022adcfcf1265bc9e2
paulozxv322-rgb <pauloxzv322@gmail.com>
2026-02-24 19:45:30 -0300

    teste

---
+++ src/main/java/com/projeto/consultaclinica/ConsultaclinicaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.consultaclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaclinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaclinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/consultaclinica/ConsultaclinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.consultaclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaclinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
